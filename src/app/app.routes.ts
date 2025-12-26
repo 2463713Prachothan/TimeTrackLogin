ee1e7d1049a3a675ab53001fe744df85eb9c26d7
S <2463711@cognizant.com>
2025-12-25 22:38:11 -0800

    landing page-hero

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '../components/home/home.component';
+import { HomeComponent } from './roles/auth/home/home.component';
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
