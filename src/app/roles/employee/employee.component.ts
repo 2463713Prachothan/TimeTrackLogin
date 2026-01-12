c0be0e214b5b9f957e2b5323ba093949aabe6118
Umesh <umeshkargvel@gmail.com>
2026-01-12 10:46:09 +0530

    created personal reports

--- src/app/roles/employee/employee.component.ts
+++ src/app/roles/employee/employee.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { DashboardemployeeComponent } from "./dashboardemployee/dashboardemployee.component";
-import { TasksComponent } from "./tasksassigned/tasksassigned.component";
 
 @Component({
   selector: 'app-employee',
-  imports: [DashboardemployeeComponent, TasksComponent],
+  imports: [],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
--- src/app/roles/employee/personalreports/personalreports.component.ts
+++ src/app/roles/employee/personalreports/personalreports.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component , OnInit, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 @Component({
-  selector: 'app-personal-reports', // This MUST match the tag in your HTML
+  selector: 'app-personalreports',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './personalreports.component.html',
   styleUrls: ['./personalreports.component.css']
 })
-export class PersonalreportsComponent { } // Note the capitalized 'P'
+export class PersonalreportsComponent implements AfterViewInit{ 
+ @ViewChild('barChart') barChartCanvas!: ElementRef;
+  @ViewChild('pieChart') pieChartCanvas!: ElementRef;
+
+  ngAfterViewInit() {
+    this.createBarChart();
+    this.createPieChart();
+  }
+
+  createBarChart() {
+    new Chart(this.barChartCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['Dec 13', 'Dec 14', 'Dec 15'],
+        datasets: [{
+          label: 'Hours Logged',
+          data: [7.8, 8.2, 7.5],
+          backgroundColor: '#8cc63f', // Deep purple from your design
+          borderRadius: 8
+        }]
+      },
+      options: { responsive: true, maintainAspectRatio: false }
+    });
+  }
+
+  createPieChart() {
+    new Chart(this.pieChartCanvas.nativeElement, {
+      type: 'pie',
+      data: {
+        labels: ['Completed', 'In Progress', 'Pending'],
+        datasets: [{
+          data: [33, 33, 33],
+          backgroundColor: ['#10b981', '#3b82f6', '#64748b'] // Match your status colors
+        }]
+      },
+      options: { responsive: true, maintainAspectRatio: false }
+    });
+  }
+} // Note the capitalized 'P'
